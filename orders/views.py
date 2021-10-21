38fb2ba9d5c5732b9fec609471df626144fc50d3
임종성 <qkqndudnxld@gmail.com>
2021-10-21 15:50:07 +0900

    Add: Update Recipt View

--- orders/views.py
+++ orders/views.py
@@ -25,6 +25,7 @@ class ReceiptView(View):
             for item in items:
                 item_id = item['item_id']
                 qty = item['quantity']
+
                 if not Item.objects.filter(id=item_id).exists():
                     Item.objects.create(id = item_id, qty = qty)
                 else:
@@ -35,6 +36,43 @@ class ReceiptView(View):
 
             return JsonResponse({'MESSAGE': "SUCCESS"}, status=201)
 
+    @query_debugger
     def put(self, request):
         with transaction.atomic():
             data = json.loads(request.body)
+
+            order_id = data['order_id']
+            type = data['type']
+            items = data['items']
+            
+            order_details = OrderDetail.objects.filter(order_id = order_id)
+            for order_detail in order_details:
+                item = Item.objects.get(id = order_detail.item_id)
+                item.qty -= order_detail.qty
+                item.save()
+                order_detail.qty = 0
+                order_detail.save()
+
+            for item in items:
+                item_id = item['item_id']
+                qty = item['quantity']
+
+                if not Item.objects.filter(id=item_id).exists():
+                    Item.objects.create(id = item_id, qty = qty)
+                else:
+                    old_item = Item.objects.get(id=item_id)
+                    old_item.qty += qty
+                    old_item.save()
+                if not OrderDetail.objects.filter(order_id = order_id, item_id=item_id).exists():
+                    OrderDetail.objects.create(item_id = item_id, qty=qty, order_id=order_id)
+                else:
+                    order_detail = OrderDetail.objects.get(order_id = order_id, item_id = item_id)
+                    order_detail.qty = qty
+                    order_detail.save()
+                    
+            return JsonResponse({'MESSAGE': "SUCCESS"}, status=201)
+            
+
+
+
+            
