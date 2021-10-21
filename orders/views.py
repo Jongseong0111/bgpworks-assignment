c817821e2383556654ce9fce47e8fb2422e0e4f6
임종성 <qkqndudnxld@gmail.com>
2021-10-21 21:33:35 +0900

    Update: 입고/출고서 수정 로직

--- orders/views.py
+++ orders/views.py
@@ -22,7 +22,6 @@ class ReceiptView(View):
                 time = time, 
                 type = type
                 )
-
             
             # 생성한 order의 id로 order detail 배열 생성
             new_order_detail = []
@@ -61,49 +60,7 @@ class ReceiptView(View):
                 ids[index].qty += old_qty[index]
                 
             Item.objects.bulk_update(ids, ['qty'])
-
-
-
-            # ### 
-
-            # id_list = [item['item_id'] for item in items]
-            # new_item = []
-            # old_item = []
-            # new_order_detail = []
-
             
-            # for item in items:
-            #     item_id = item['item_id']
-            #     qty = item['quantity']
-                
-            #     if not Item.objects.filter(item_id = item_id).exists():
-            #         new_item.append(Item(item_id = item_id, qty = qty))
-
-            #     else:
-            #         old_item.append((item_id, qty))
-            # # 존재하지 않던 Item 생성
-            # Item.objects.bulk_create(new_item)
-
-            # if old_item!=[]:
-            #     ids = [i[0] for i in old_item]
-            #     qtys = [i[1] for i in old_item]
-            #     old_items = Item.objects.filter(item_id__in = ids)
-
-            #     for i in range(len(old_items)):
-            #         old_items[i].qty += qtys[i]
-            
-            #     Item.objects.bulk_update(old_items, ['qty'] )
-            # print(id_list)
-            # item_ids = Item.objects.filter(item_id__in = id_list)
-            # print(len(item_ids))
-            # id_lists = [item.id for item in item_ids]
-            # for i in range(len(items)):
-            #     id = id_lists[i]
-            #     qty = items[i]['quantity']
-            #     # Order Detail를 생성하기 위해 배열에 담기
-            #     new_order_detail.append(OrderDetail(order_id = order.id, item_id = id, qty = qty))
-
-            # OrderDetail.objects.bulk_create(new_order_detail)
             return JsonResponse({'MESSAGE': "SUCCESS"}, status=201)
 
     @query_debugger
@@ -114,36 +71,86 @@ class ReceiptView(View):
             order_id = data['order_id']
             type = data['type']
             items = data['items']
-            
-            order_details = OrderDetail.objects.filter(order_id = order_id)
-            for order_detail in order_details:
-                item = Item.objects.get(id = order_detail.item_id)
-                item.qty -= order_detail.qty
-                item.save()
-                order_detail.qty = 0
-                order_detail.save()
+            pair = {}
+            for item in items:
+                pair[item['item_id']] = item['quantity']
 
+            
+            # order_id 기준으로 order detail 불러오기
+
+            order_details = OrderDetail.objects.filter(order_id = order_id)
+
+            ids = [order_detail.item_id for order_detail in order_details] # 존재하던 order detail의 item_id
+            qtys = [order_detail.qty for order_detail in order_details] # 존재하던 order detail의 qty
+
+            remove_items = []
+            # 원복하기 위해 item 불러와서 수정
+            old_items = Item.objects.filter(item_id__in = ids) # 
+            for i in range(len(old_items)):
+                old_items[i].qty -= qtys[i] # 기존 order detail의 수량만큼 감소
+            
+
+            # Item bulk_update
+            Item.objects.bulk_update(old_items, ['qty'])
+            
+            # 기존 order detail은 update, 새로 들어온 것 중 없는 것은 create
+
+            new_ids = [item['item_id'] for item in items] # 새로 들어온 order의 item_id
+            for order_detail in order_details:
+                if order_detail.item_id not in new_ids: 
+                    order_detail.qty = 0 # 삭제되는 order detail 처리
+                else:
+                    order_detail.qty = pair[order_detail.item_id] # 업데이트되는 order detail 처리
+            
+            # Update order detail
+            OrderDetail.objects.bulk_update(order_details, ['qty'])
+
+            # Create order detail
+            new_order_detail = []
+            for item in items:
+                item_id = item['item_id']
+                qty = item['quantity']
+                if item_id not in ids: # 추가되는 order detail
+                    new_order_detail.append(OrderDetail(order_id = order_id, item_id = item_id, qty = qty))
+
+            OrderDetail.objects.bulk_create(new_order_detail)
+
+            # 존재하는 id 뽑아내기
+            ids_list = [item['item_id'] for item in items]
+
+            ids = Item.objects.filter(item_id__in = ids_list)
+            old_ids = [item.item_id for item in ids]
+            old_qty = []
+
+            # 존재하는 id는 update, 존재하지 않는 id는 create 배열에 나눠담기
+            new_items = []
+            old_items = []
             for item in items:
                 item_id = item['item_id']
                 qty = item['quantity']
 
-                if not Item.objects.filter(id=item_id).exists():
-                    Item.objects.create(id = item_id, qty = qty)
+                if item_id in old_ids:
+                    old_items.append(item_id)
+                    old_qty.append(qty)
                 else:
-                    old_item = Item.objects.get(id=item_id)
-                    old_item.qty += qty
-                    old_item.save()
-                if not OrderDetail.objects.filter(order_id = order_id, item_id=item_id).exists():
-                    OrderDetail.objects.create(item_id = item_id, qty=qty, order_id=order_id)
-                else:
-                    order_detail = OrderDetail.objects.get(order_id = order_id, item_id = item_id)
-                    order_detail.qty = qty
-                    order_detail.save()
+                    new_items.append(Item(item_id = item_id, qty = qty) )
             
+            # 존재하지 않는 id에 대해 bulk_create
+            Item.objects.bulk_create(new_items)
+            
+            # 존재하는 id에 대해 bulk_update
+            for index in range(len(ids)):
+                ids[index].qty += old_qty[index]
+                
+            Item.objects.bulk_update(ids, ['qty'])
+
+
+
+            ###
             result = []
-            stocks = Item.objects.exclude(qty=0)
+            stocks = Item.objects.exclude(qty=0).order_by('item_id')
             for stock in stocks:
-                result.append({"item_id" : stock.id, "quantity" : stock.qty})
+                result.append({"item_id" : stock.item_id, "quantity" : stock.qty})
 
             return JsonResponse({'RESULT': result}, status=200)
             
